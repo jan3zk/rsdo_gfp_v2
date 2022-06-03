ea531d5df8c4e0a68bcd1feba5f567cfb534098c
jan3zk <janez.krizzaj@gmail.com>
2022-06-03 12:57:14 +0200

    Added pretrained model switch.

--- DeepPhonemizer_g2p.py
+++ DeepPhonemizer_g2p.py
@@ -7,6 +7,8 @@ from dp.train import train
 from dp.phonemizer import Phonemizer
 import numpy as np
 from Levenshtein import distance as lev
+import difflib
+import collections
 
 
 def training(model_path, train_dict):
@@ -58,20 +60,28 @@ def testing(model_path, test_dict):
   # Evaluation
   str_err = 0
   sym_err = 0
+  errs = []
   print('\nNapačne pretvorbe na testnih nizih:')
   for gr, ref, pred in zip(test_gr, test_ph, result.phonemes):
     if pred != ref:
-      # ~ import ipdb; ipdb.set_trace()
+      err = [li for li in difflib.ndiff(ref, pred) if li[0] != ' ']
+      errs.append([err[i * 2:(i + 1) * 2] for i in range((len(err) + 2 - 1) // 2 )])
       str_err = str_err + 1
       sym_err = sym_err + lev(ref,pred)
-      print('%s,\t%s,\t%s'%(gr,ref,pred))
+      print('%s,\t%s,\t%s,\t%s'%(gr,ref,pred,err))
+  errs = [item for sublist in errs for item in sublist]
+  errs = [' '.join([str(c) for c in lst]) for lst in errs]
+  counter = collections.Counter(errs)
+  print('\nDeset najpogostejših napak:',*counter.most_common(10),sep='\n')
+  counter.most_common()
   str_err = str_err/len(test_ph)*100
   sym_err = sym_err/len(''.join(test_ph))*100
   print('\nRezultati na %s:'%test_dict)
   print('WER=%.1f%%, PER=%.1f%%'%(str_err,sym_err))
 
 def main(args):
-  # ~ training(args.model, args.train_dict)
+  if not args.use_pretrained:
+    training(args.model, args.train_dict)
   testing(args.model, args.test_dict)
 
 if __name__ == '__main__':
@@ -88,11 +98,9 @@ if __name__ == '__main__':
     type = str,
     default = '/storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_ASR_LemmaSplit_v2/sloleks_test_gfp.dict'
   )
+  parser.add_argument('--use_pretrained',
+    action='store_true',
+  )
   args = parser.parse_args()
   
   main(args)
-
-# CUDA_VISIBLE_DEVICES=1 python DeepPhonemizer_g2p_v3.py --model ./results/DeepPhonemizer/SingleCharacters/SloLeks_Validated_ASR_RandomSplit/ --train_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_ASR_RandomSplit/sloleks_train_gfp.dict --test_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_ASR_RandomSplit/sloleks_test_gfp.dict
-# CUDA_VISIBLE_DEVICES=1 python DeepPhonemizer_g2p_v3.py --model ./results/DeepPhonemizer/SingleCharacters/SloLeks_Validated_ASR_LemmaSplit_v2/ --train_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_ASR_LemmaSplit_v2/sloleks_train_gfp.dict --test_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_ASR_LemmaSplit_v2/sloleks_test_gfp.dict
-# CUDA_VISIBLE_DEVICES=1 python DeepPhonemizer_g2p_v3.py --model ./results/DeepPhonemizer/SingleCharacters/SloLeks_Validated_TTS_RandomSplit/ --train_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_TTS_RandomSplit/sloleks_train_gfp.dict --test_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_TTS_RandomSplit/sloleks_test_gfp.dict
-# CUDA_VISIBLE_DEVICES=1 python DeepPhonemizer_g2p_v3.py --model ./results/DeepPhonemizer/SingleCharacters/SloLeks_Validated_TTS_LemmaSplit_v2/ --train_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_TTS_LemmaSplit_v2/sloleks_train_gfp.dict --test_dict /storage/rsdo/jtdh/SingleCharacters/SloLeks_Validated_TTS_LemmaSplit_v2/sloleks_test_gfp.dict
